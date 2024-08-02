36d977c092be4f8e9cc95f141d63d891752f003e
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-02 16:54:57 +0000

    automated commit by check50 [check50=True]

--- faces.py
+++ faces.py
@@ -1,5 +1,6 @@
 def main():
     text=input("Enter a smiley or sad face")
-def emoji(text1):
-    return text1.replace(":)","🙂").replace(":(","🙁")
+    def emoji(text1):
+        return text1.replace(":)","🙂").replace(":(","🙁")
+    emoji()
 main()
