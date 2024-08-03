977737070106a69c61eae87261dff823f60f7798
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-03 04:14:04 +0000

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -1,5 +1,5 @@
 Question=input("What is the Answer to the Great Question of Life, the Universe and Everything?").lower().strip()
-if Question=="42" or Question=="fortytwo" or Question=="forty-two":
+if Question=="42" or Question=="forty two" or Question=="forty-two":
     print("Yes")
 else:
     print("No")
