c0bb930f376b828bc704ca0e7864349593acca1c
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-03 14:44:52 +0000

    automated commit by check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -1,15 +1,15 @@
 def main():
     file_name=input("Enter the file name").lower().strip()
-    gif = "image/gif"
-    jpg = "image/jpeg"
-    jpeg = "image/jpeg"
-    png = "image/png"
-    pdf = "application/pdf"
-    txt = "text/plain"
-    zip = "application/zip"
+    gif="image/gif"
+    jpg="image/jpeg"
+    jpeg="image/jpeg"
+    png="image/png"
+    pdf="application/pdf"
+    txt="text/plain"
+    zip="application/zip"
     if file_name.endswith(".gif"):
         print(gif)
-    elif file_name.endswith(".jpg")  or extension.endswith(".jpeg"):
+    elif file_name.endswith(".jpg")or extension.endswith(".jpeg"):
         print(jpg)
     elif file_name.endswith(".png"):
         print(png)
