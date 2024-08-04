fb2024b4139a4711e4686e1e4e663176092d531e
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-04 10:36:37 +0000

    automated commit by check50 [check50=True]

--- plates.py
+++ plates.py
@@ -5,14 +5,15 @@ def main():
     else:
         print("Invalid")
 def is_valid(s):
-    if 6>=len(s) and s[0:2].isalpha() and s.isalnum():
-        for char in s:
-            if char.isdidgit():
-                index=s.index(char)
-                if s[index:].isdigit() and int(char)!=0:
-                    return True
-                else:
-                    return False
+    if 6>=len(s)>=2 and s.isalpha():
         return True
+        if s.alnum() and s[0:2].isalpha():
+            for char in s:
+                if char.isdidgit():
+                    index=s.index(char)
+                    if s[index:].isdigit() and int(char)!=0:
+                        return True
+                    else:
+                        return False
 if __name__=="__main__":
     main()
