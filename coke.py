e8ccd355f6381473614469109ac56ffeaeb48a8a
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-04 06:07:03 +0000

    automated commit by check50 [check50=True]

--- coke.py
+++ coke.py
@@ -6,6 +6,6 @@ while amount_due>0:
     if coin==25 or coin==10 or coin==5:
         amount_due-=coin
         print(amount_due)
-    if amount_due<=0:
+        if amount_due<=0:
         change_owed-=amount_due
         print(change_owed)
