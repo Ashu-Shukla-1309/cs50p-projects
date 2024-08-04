289b02aabf0324f745aa76e87688f9500af1c55f
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-04 05:19:33 +0000

    automated commit by check50 [check50=True]

--- meal.py
+++ meal.py
@@ -1,1 +1,15 @@
-print("L")
+def main():
+    Time=input("What time is it?")
+    answer=convert(time)
+    if time>=7 and time<=8:
+        print("breakfast time")
+    elif time>=12 and time<=13:
+        print("lunch time")
+    elif time>=18 and time<=19:
+        print("dinner time")
+def convert(time):
+    hours,minutes=time.split(":")
+    minute=float(minutes)/60
+    return float(hours)+minute
+if __name__ == "__main__":
+    main()
