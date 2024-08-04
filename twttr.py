61bcd4167a034bc22ea957ba91df11d60607d572
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-04 06:42:56 +0000

    automated commit by check50 [check50=True]

--- twttr.py
+++ twttr.py
@@ -1,10 +1,11 @@
 def main():
     text=input("")
-    result=remove_vowels(text)
-    print(result)
+    output=remove_vowels(text)
+    print(output)
 def remove_vowels(text):
     vowels="AEIUOaeiou"
     result=""
     for char in vowels:
         result+=char
+        return result
 main()
