7377fb3179e7fba15a500abb941b38c7db700cbe
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-04 11:01:27 +0000

    automated commit by check50 [check50=True]

--- plates.py
+++ plates.py
@@ -5,26 +5,26 @@ def main():
     else:
         print("Invalid")
 def is_valid(s):
-    valid_check = True
-    digit_count = 0
+    validity= True
+    count_digit = 0
     if(len(s) < 2 or len(s) > 6 ):
-        valid_check = False
-        return valid_check
+        validity = False
+        return validity
     if(s[0].isdigit() or s[1].isdigit()):
-        valid_check = False
-        return valid_check
+        validity = False
+        return validity
     for char in s:
         if(not char.isalnum()):
-            valid_check = False
-            return valid_check
-        if(digit_count > 1 and char.isalpha()):
-            valid_check = False
-            return valid_check
+            validity = False
+            return validity
+        if(count_digit > 1 and char.isalpha()):
+            validity = False
+            return validity
         if(char.isdigit()):
-            digit_count += 1
-        if(digit_count == 1 and char == "0"):
-            valid_check = False
-            return valid_check
-    return valid_check
+            count_digit += 1
+        if(count_digit == 1 and char == "0"):
+            validity = False
+            return validity
+    return validity
 
 main()
