e3f442147b3c6ad1a4422369a4a199d265e7e16d
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-05 10:37:58 +0000

    automated commit by check50 [check50=True]

--- grocery.py
+++ grocery.py
@@ -1,9 +1,10 @@
 dict1={}
 while True:
     try:
-        input1=input("Enter your items").upper()
+        input1=input("Enter your items")
         if input1="":
             break
+        input1=input1.upper()
         if input1 in dict1:
             dict1[input1]+=1
         else:
@@ -13,5 +14,4 @@ while True:
         break
 for input1 in sorted(dict1.keys()):
     print(dict1[input1],input1)
-
-
+exit(0)
