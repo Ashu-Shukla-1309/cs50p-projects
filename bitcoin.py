d549281757caf72792bf2e7053d3b2b19a41f743
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-14 13:51:03 +0000

    automated commit by check50 [check50=True]

--- bitcoin.py
+++ bitcoin.py
@@ -3,16 +3,16 @@ import requests
 if len(sys.argv)== 2:
     url="https://api.coindesk.com/v1/bpi/currentprice.json"
     try:
-        response=requests.get("url")
-        json_response=response.json()
+        request=requests.get("url")
+        json_request=request.json()
         n=float(sys.argv[1])
     except ValueError:
         sys.exit("Command line argument is not a number")
     except requests.RequestException:
         print("HTTP request failed")
     else:
-        coin_price=json_response['bpi']['USD']['rate_float']
-        amount=n*coin_price
-        print(f"${amount:,.4f}")
+        coin=json_request['bpi']['USD']['rate_float']
+        Total=n*coin
+        print(f"${Total:,.4f}")
 else:
     sys.exit("missing command line argument")
