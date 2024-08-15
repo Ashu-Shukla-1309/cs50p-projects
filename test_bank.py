ad81c16899fe6b32c2797dc46f1fbdb96e049290
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-15 06:30:13 +0000

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -9,7 +9,7 @@ def test_value1():
     assert value("hello")==0
 def test_value2():
     assert value("hi")==20
-    assert value("hiak")==20
+    assert value("hlak")==20
 def test_value3():
     assert value("cs50")==100
     assert value("50")==100
