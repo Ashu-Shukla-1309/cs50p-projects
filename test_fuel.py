ff2b2e7ff49af69fb84e6da2c4891d62deb0b8ae
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-15 11:22:28 +0000

    automated commit by check50 [check50=True]

--- test_fuel.py
+++ test_fuel.py
@@ -8,9 +8,9 @@ def test_convert():
     assert convert("1/4")==25
     assert convert("1/100")==1
     assert convert("99/100")==99
-    with pytest.raise(ValueError):
+    with pytest.raises(ValueError):
         convert("cat/dog")
-    with pytest.raise(ZeroDivisionError):
+    with pytest.raises(ZeroDivisionError):
         convert("1/0")
 def test_gauge():
     assert gauge(50)=="50%"
