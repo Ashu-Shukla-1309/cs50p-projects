e8dc9eae540e379371e70809f4044e0253efdc62
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-17 10:42:49 +0000

    automated commit by check50 [check50=True]

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -4,10 +4,10 @@ def main():
     test_ip_false()
 def test_ip_true():
     assert validate("123.123.123.123")==True
-    assert validate("12.13.34.123")==True
+    assert validate("12.13.3.123")==True
 def test_ip_false():
-    assert validate("2236.123.123.123")==False
-    assert validate("6.123.123.123")==False
+    assert validate("1234.123.123.123")==False
+    assert validate("2.666.666.666")==False
     assert validate("cat")==False
 if __name__ == "__main__":
     main()
