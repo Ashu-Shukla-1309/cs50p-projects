6ce1fb29f4bbf33da71cd9a233e75e26d88c557f
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-23 15:32:27 +0000

    automated commit by check50 [check50=True]

--- test_working.py
+++ test_working.py
@@ -7,15 +7,15 @@ def main():
     wrong_minute()
 def wrong_Format():
     with pytest.raises(ValueError):
-        convert("9 AM-9 PM")
+        convert("9 AM - 9 PM")
 def test_time():
-    assert convert("9 AM-5 PM")=="9:00 to 17:00"
-    assert convert("10 PM-8 AM")=="22:00 to 08:00"
-    assert convert("10:30 PM-8:50 AM")=="22:30 to 08:50"
+    assert convert("9 AM to 5 PM")=="9:00 to 17:00"
+    assert convert("10 PM to 8 AM")=="22:00 to 08:00"
+    assert convert("10:30 PM to 8:50 AM")=="22:30 to 08:50"
 def wrong_hour():
     with pytest.raises(ValueError):
-        convert("13 PM-17 PM")
+        convert("13 PM to 17 PM")
 def wrong_minute():
     with pytest.raises(ValueError):
-        convert("9:60 AM-9:60 PM")
+        convert("9:60 AM to 9:60 PM")
 
