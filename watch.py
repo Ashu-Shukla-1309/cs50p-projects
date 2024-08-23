658a9707d32ae64ca573db2a50394c7f3fed6259
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-23 14:25:14 +0000

    automated commit by check50 [check50=True]

--- watch.py
+++ watch.py
@@ -3,7 +3,7 @@ import sys
 def main():
     print(parse(input("HTML: ")))
 def parse(s):
-    if re.search(r"<iframe(.)*><\/iframe>.s"):
+    if re.search(r"<iframe(.)*><\/iframe>,s"):
         pattern=re.search(r"(http(s)*:\/\/(www\.)*youtube\.com\/embed\/)([a-z_A-Z_0-9]+)",s)
         if pattern:
             url=pattern.groups()
