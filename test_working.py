66327e109e8f346d50953b2e335e1c841381619f
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-23 16:30:07 +0000

    automated commit by check50 [check50=True]

--- test_working.py
+++ test_working.py
@@ -14,5 +14,3 @@ def test_invalid_cases():
         convert("9 AM - 5 PM")
     with pytest.raises(ValueError):
         convert("09:00 AM - 17:00 PM")
-    with pytest.raises(ValueError):
-        convert("8:60 AM to "4:60 PM")
