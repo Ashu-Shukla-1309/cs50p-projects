8bfd1f9c3777d2cc1133969538188c534970f8c4
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-25 10:40:26 +0000

    automated commit by check50 [check50=True]

--- um.py
+++ um.py
@@ -3,7 +3,7 @@ def main():
     print(count(input("Text: ")))
 def count(s):
     pattern=r"\bum\b"
-    match=re.findall(pattern,s,re.IGNORECASE)
+    match=re.findall(pattern,s)
     return len(match)
 if __name__ == "__main__":
     main()
