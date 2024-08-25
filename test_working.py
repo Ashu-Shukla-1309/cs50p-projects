522c988571099414db79b9d22533014f153120d8
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-25 04:33:51 +0000

    automated commit by check50 [check50=True]

--- test_working.py
+++ test_working.py
@@ -7,8 +7,17 @@ def test_valid_times():
     assert convert("12:00 PM to 5:00 AM") == "12:00 to 05:00"
 def test_invalid_times():
     with pytest.raises (ValueError):
-        convert("13:00 AM to 5:00 PM")
+        convert("13:00 AM To 5:00 PM")
     with pytest.raises (ValueError):
         convert("9:00 AM to 55:00 PM")
     with pytest.raises (ValueError):
         convert("9:00 AM to 5:60 PM")
+def test_invalid_formats():
+    with pytest.raises (ValueError):
+        convert("9:00ΑΜ ΤΟ 5:00PM")
+    with pytest.raises (ValueError):
+        convert("9:00 to 5:00")
+    with pytest.raises (ValueError):
+        convert("9:00 AM 5:00 PM")
+    with pytest.raises (ValueError):
+        convert("9:00 am to 5:00 pm")
