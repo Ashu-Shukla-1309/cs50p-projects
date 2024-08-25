8364d6f4fe4b17056256f9fccdf70a01b9bf4890
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-25 10:44:50 +0000

    automated commit by check50 [check50=True]

--- um.py
+++ um.py
@@ -1,6 +1,6 @@
 import re
 def main():
-    print(count(input("Text: ")))
+    print(count(input("Text: ")).strip())
 def count(s):
     pattern=r"\bum\b"
     match=re.findall(pattern,s,re.IGNORECASE)
