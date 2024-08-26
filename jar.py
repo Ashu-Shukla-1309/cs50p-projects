79dcf6eb593740850813b9ba74d19d6b6cb0fb64
Asfgghhhh <Asfgghhhh@users.noreply.github.com>
2024-08-26 13:24:46 +0000

    automated commit by check50 [check50=True]

--- jar.py
+++ jar.py
@@ -4,24 +4,19 @@ class Jar:
             raise ValueError("Capacity must be a non-negative integer")
         self._capacity = capacity
         self._cookies = 0
-
     def __str__(self):
         return "🍪" * self._cookies
-
     def deposit(self, n):
         if self._cookies + n > self._capacity:
             raise ValueError("Exceeds capacity")
         self._cookies += n
-
     def withdraw(self, n):
         if n > self._cookies:
             raise ValueError("Not enough cookies")
         self._cookies -= n
-
     @property
     def capacity(self):
         return self._capacity
-
     @property
     def size(self):
         return self._cookies
